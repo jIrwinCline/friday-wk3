f6c3f0952d8202f3540542f107ea0da31a22b96d
jirwincline <jakobinwins@gmail.com>
2019-06-14 09:57:29 -0700

    Fix issues with header

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+function changeArr(inputNum) {
+  //// for loop to create a number in an array for each number less than input number
+  for(var i=inputNum; i>0; i--) {
+    var numberArr = [];
+    numberArr.push(i);
+  }
+
+}
+
+
+
+
+
+
+/////////////////////////////////////
 $(document).ready(function(){
   $('form').submit(function(event) {
 
