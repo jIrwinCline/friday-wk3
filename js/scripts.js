903bd8e74106d47adcab85c3e652e049b380180e
jirwincline <jakobinwins@gmail.com>
2019-06-14 10:39:35 -0700

    add to html and functionality to first function in js to create an array based on input

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
 function changeArr(inputNum) {
   //// for loop to create a number in an array for each number less than input number
+  var numberArr = [];
   for(var i=inputNum; i>0; i--) {
-    var numberArr = [];
     numberArr.push(i);
   }
+  return numberArr;
 
 }
 
@@ -15,6 +16,12 @@ function changeArr(inputNum) {
 /////////////////////////////////////
 $(document).ready(function(){
   $('form').submit(function(event) {
-
+    event.preventDefault();
+    var value = $("input").val();
+    console.log("here " + value);
+    var array1 = changeArr(value);
+    console.log(array1)
+    $(".results").text(value);
   });
+
 });
